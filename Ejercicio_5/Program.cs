6530b78325f11e340cba35c183c3ab075b09fd92
Usuario <Usuario@DESKTOP-BIRK270>
2022-08-28 16:20:55 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Ejercicio_5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int pos = 5;
+            Console.WriteLine(numerofibonacci(pos));
+        }
+        
+        public static int numerofibonacci(int pos)
+        {
+            if (pos > 0)
+            {
+                int numero1 = 0, numero2 = 1, suma = 1;
+                for (int i = 1; i < pos; i++)
+                {
+                    suma = numero1 + numero2;
+                    numero1 = numero2;
+                    numero2 = suma;
+                }
+
+                return suma;
+            }
+            return -1;                           
+        }
+
+    }
+}
